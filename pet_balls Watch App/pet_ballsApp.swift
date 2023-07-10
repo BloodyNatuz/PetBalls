81e04b5e56afa585e31461f0d9f0806fdacadffe
Nathuz <nathuznoepupin@gmail.com>
2023-07-10 09:28:09 +0200

    🎉 First commit

---
+++ pet_balls Watch App/pet_ballsApp.swift
@@ -0,0 +1,17 @@
+//
+//  pet_ballsApp.swift
+//  pet_balls Watch App
+//
+//  Created by Noé Pupin on 10/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct pet_balls_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
